cf3255c39a37074c306d402c4654a5f0e508818d
Max <masterplayer@yandex.ru>
2026-08-17 10:32:17 +0300

    add matlab script for generating ROM files for systemverlog component
    for twiddle ROM organization we use files for coefficients

---
+++ scripts/generate_twiddle_rom.m
@@ -0,0 +1,29 @@
+clc;
+clear all;
+close all;
+
+NFFT = 256;
+DATA_WIDTH = 16;
+
+limit = (2^(DATA_WIDTH-1))-1;
+
+k = 0:1:(NFFT/2)-1;
+twiddle_cos = int32(round(cos(2*pi*k/NFFT)*limit))';
+twiddle_sin = int32(round(-sin(2*pi*k/NFFT)*limit))';
+
+twiddle_cos_hex = dec2hex(twiddle_cos)';
+twiddle_sin_hex = dec2hex(twiddle_sin)';
+
+filename_real = strcat('../outputs/twiddle_real_hex_', int2str(NFFT), '.hex');
+filename_imag = strcat('../outputs/twiddle_imag_hex_', int2str(NFFT), '.hex');
+
+fileID = fopen(filename_real, 'w');
+fprintf(fileID, '%c%c%c%c ', twiddle_cos_hex);
+fclose(fileID);
+
+fileID = fopen(filename_imag, 'w');
+fprintf(fileID, '%c%c%c%c ', twiddle_sin_hex);
+fclose(fileID);
+
+
+
