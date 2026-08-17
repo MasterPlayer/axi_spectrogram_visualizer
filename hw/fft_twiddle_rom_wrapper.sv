1fc139aa7ccf2a707481ff0fe68bbc76906ad611
Max <masterplayer@yandex.ru>
2026-08-17 10:31:30 +0300

    twiddle ROM loadable from memory

--- hw/fft_twiddle_rom_wrapper.sv
+++ hw/fft_twiddle_rom_wrapper.sv
@@ -1,7 +1,9 @@
 
 module fft_twiddle_rom_wrapper #(
-    DATA_WIDTH = 16,
-    DATA_DEPTH = 32
+    parameter DATA_WIDTH     = 16                                                                                      ,
+    parameter DATA_DEPTH     = 32                                                                                      ,
+    parameter DATA_PATH_REAL = "z:/fpga_development/github/axi_spectrogram_visualizer/outputs/twiddle_real_hex_256.hex",
+    parameter DATA_PATH_IMAG = "z:/fpga_development/github/axi_spectrogram_visualizer/outputs/twiddle_imag_hex_256.hex"
 ) (
     input  logic                          clk   ,
     input  logic [$clog2(DATA_DEPTH)-1:0] i_addr,
@@ -10,8 +12,9 @@ module fft_twiddle_rom_wrapper #(
 );
 
     fft_twiddle_rom_real #(
-        .DATA_WIDTH(DATA_WIDTH),
-        .DATA_DEPTH(DATA_DEPTH)
+        .DATA_WIDTH(DATA_WIDTH    ),
+        .DATA_DEPTH(DATA_DEPTH    ),
+        .DATA_PATH (DATA_PATH_REAL)
     ) fft_twiddle_rom_real_inst (
         .clk   (clk   ),
         .i_addr(i_addr),
@@ -19,8 +22,9 @@ module fft_twiddle_rom_wrapper #(
     );
 
     fft_twiddle_rom_imag #(
-        .DATA_WIDTH(DATA_WIDTH),
-        .DATA_DEPTH(DATA_DEPTH)
+        .DATA_WIDTH(DATA_WIDTH    ),
+        .DATA_DEPTH(DATA_DEPTH    ),
+        .DATA_PATH (DATA_PATH_IMAG)
     ) fft_twiddle_rom_imag_inst (
         .clk   (clk   ),
         .i_addr(i_addr),
